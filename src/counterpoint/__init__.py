d27ada1decf705e6f069f4156ff1e0dd48a55e87
Matteo <matteo@giskard.ai>
2025-06-10 12:36:37 +0200

    Working on tools

---
+++ src/counterpoint/__init__.py
@@ -0,0 +1,6 @@
+from .generator import Generator
+from .pipeline import Pipeline
+from .chat import Chat, Message
+from .tools import Tool
+
+__all__ = ["Generator", "Pipeline", "Chat", "Message", "Tool"]
--- src/counterpoint/tools.py
+++ src/counterpoint/tools.py
@@ -1,6 +1,7 @@
-from typing import Literal
+import inspect
+from typing import Any, Callable, Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Function(BaseModel):
@@ -12,3 +13,37 @@ class ToolCall(BaseModel):
     id: str
     type: Literal["function"] = "function"
     function: Function
+
+
+class Tool(BaseModel):
+    name: str
+    description: str
+    parameters: dict[str, Any] = Field(default_factory=dict)
+
+    fn: Callable
+
+    @classmethod
+    def from_callable(cls, fn: Callable) -> "Tool":
+        return cls(
+            name=fn.__name__,
+            description=fn.__doc__,
+            parameters=fn.__annotations__,
+            fn=fn,
+        )
+
+    async def run(self, *args: Any, **kwargs: Any) -> Any:
+        res = self.fn(*args, **kwargs)
+        if inspect.isawaitable(res):
+            res = await res
+
+        return res
+
+    def to_litellm_function(self) -> dict[str, Any]:
+        return {
+            "type": "function",
+            "function": {
+                "name": self.name,
+                "description": self.description,
+                "parameters": self.parameters,
+            },
+        }
