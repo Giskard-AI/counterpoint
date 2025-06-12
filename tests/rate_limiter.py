8b3b6c8f57f7b385b36cc7682e87addcae59c8e6
Kevin Messiaen <kevin.messiaen@icloud.com>
2025-06-12 16:55:13 +0700

    Added test for burst

--- tests/rate_limiter.py
+++ tests/rate_limiter.py
@@ -13,6 +13,11 @@ async def mock_job(rate_limiter: RateLimiter):
     async with rate_limiter:
         return datetime.datetime.now()
 
+async def mock__long_job(rate_limiter: RateLimiter):
+    async with rate_limiter:
+        started = time.monotonic()
+        await asyncio.sleep(1)
+        return started
 
 async def test_rate_limiter_max_concurrent_requests():
     rate_limiter = RateLimiter()
@@ -45,7 +50,7 @@ async def test_rate_limiter_throttle_rate():
     for i in range(10):
         async with rate_limiter:
             pass
-        assert time.monotonic() - start_time > throttle * i and time.monotonic() - start_time < throttle * (i + 1)
+        assert time.monotonic() - start_time >= throttle * i and time.monotonic() - start_time < throttle * (i + 1)
     
     # No throttle should be applied
     await asyncio.sleep(throttle)
@@ -53,3 +58,23 @@ async def test_rate_limiter_throttle_rate():
     async with rate_limiter:
         pass
     assert time.monotonic() - start_time < throttle
+
+
+async def test_rate_limiter_burst_size():
+    rate_limiter = RateLimiter(burst_size=10)
+    throttle = 1 / 500
+
+    tasks = [asyncio.create_task(mock__long_job(rate_limiter)) for _ in range(20)]
+
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+
+    # First 10 requests should be run in burst
+    first_start = results[0]
+    for i in range(10):
+        assert results[i] - first_start >= throttle * i and results[i] - first_start < throttle * (i + 1)
+
+    # Last 10 requests should wait first task to finish
+    second_burst_start = results[10]
+    assert second_burst_start - first_start >= 1 and second_burst_start - first_start < 1 + throttle
+    for i in range(10, 20):
+        assert results[i] - second_burst_start >= throttle * (i - 10) and results[i] - second_burst_start < throttle * (i - 9)
