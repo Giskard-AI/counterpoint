fb0f437be4d9626e2182c86d3b77e1e26e198805
Matteo <matteo@giskard.ai>
2025-07-01 21:26:26 +0200

    feat: add retry policy to generators

--- src/counterpoint/generators/mixins.py
+++ src/counterpoint/generators/mixins.py
@@ -1,9 +1,14 @@
+from abc import abstractmethod
 from contextlib import nullcontext
-from typing import AsyncContextManager
+from typing import AsyncContextManager, Type, TypeVar
 
+import tenacity as t
 from pydantic import BaseModel, Field, field_validator
 
 from ..rate_limiter import RateLimiter, get_rate_limiter
+from .retry import RetryPolicy
+
+T = TypeVar("T")
 
 
 class WithRateLimiter(BaseModel):
@@ -22,3 +27,44 @@ class WithRateLimiter(BaseModel):
             return nullcontext()
 
         return self.rate_limiter.throttle()
+
+
+class WithRetryPolicy(BaseModel):
+    """Adds a retry policy to the generator."""
+
+    retry_policy: RetryPolicy | None = Field(default=RetryPolicy(max_retries=3))
+
+    @abstractmethod
+    def _should_retry(self, err: Exception) -> bool: ...
+
+    def with_retries(
+        self,
+        max_retries: int,
+        *,
+        base_delay: float | None = None,
+        exception: Type[Exception] | None = None,
+    ) -> "WithRetryPolicy":
+        params = {"max_retries": max_retries}
+
+        if base_delay is not None:
+            params["base_delay"] = base_delay
+        elif self.retry_policy is not None:
+            params["base_delay"] = self.retry_policy.base_delay
+
+        return self.model_copy(update={"retry_policy": RetryPolicy(**params)})
+
+    async def complete(self, *args, **kwargs) -> T:
+        if self.retry_policy is None:
+            return await super().complete(*args, **kwargs)
+
+        retrier = t.AsyncRetrying(
+            stop=t.stop_after_attempt(self.retry_policy.max_retries),
+            wait=t.wait_exponential(multiplier=self.retry_policy.base_delay),
+            retry=self._tenacity_retry_condition,
+            reraise=True,
+        )
+
+        return await retrier(super().complete, *args, **kwargs)
+
+    def _tenacity_retry_condition(self, retry_state: t.RetryCallState) -> bool:
+        return self._should_retry(retry_state.outcome.exception())
