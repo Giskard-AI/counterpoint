7e04a8b057f3b0b63b6c1980d6463a0517c2114b
Matteo <matteo@giskard.ai>
2025-10-13 08:14:57 +0200

    feat: namespaced templates (#49)
    * feat: namespaced templates
    
    minimal implementation
    
    * add public API
    
    * Fixed tests
    
    * Fixed liniting
    
    ---------
    
    Co-authored-by: Kevin Messiaen <kevin.messiaen@icloud.com>

--- src/counterpoint/__init__.py
+++ src/counterpoint/__init__.py
@@ -3,7 +3,14 @@ from .context import RunContext
 from .errors import Error, WorkflowError
 from .generators import Generator
 from .rate_limiter import RateLimiter, RateLimiterStrategy
-from .templates import MessageTemplate, get_prompts_manager, set_prompts_path
+from .templates import (
+    MessageTemplate,
+    get_prompts_manager,
+    set_prompts_path,
+    set_default_prompts_path,
+    add_prompts_path,
+    remove_prompts_path,
+)
 from .tools import Tool, tool
 from .workflow import ChatWorkflow, ErrorPolicy
 
@@ -16,6 +23,9 @@ __all__ = [
     "tool",
     "MessageTemplate",
     "set_prompts_path",
+    "set_default_prompts_path",
+    "add_prompts_path",
+    "remove_prompts_path",
     "get_prompts_manager",
     "RateLimiterStrategy",
     "RateLimiter",
